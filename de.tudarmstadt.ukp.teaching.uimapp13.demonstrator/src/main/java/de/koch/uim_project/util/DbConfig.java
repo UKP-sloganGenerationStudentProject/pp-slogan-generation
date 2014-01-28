0a9991ca65c689fef47b9c2519a011f0e30bab0f
FrerikKo@gmail.com <FrerikKo@gmail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-01-28 11:55:43 +0000

    Minor changes

--- de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/koch/uim_project/util/DbConfig.java
+++ de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/koch/uim_project/util/DbConfig.java
@@ -42,4 +42,51 @@ public class DbConfig {
 		return pass;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 * Eclipse generated
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * eclipse generated
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DbConfig other = (DbConfig) obj;
+		if (pass == null) {
+			if (other.pass != null)
+				return false;
+		} else if (!pass.equals(other.pass))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
+	
+
 }
