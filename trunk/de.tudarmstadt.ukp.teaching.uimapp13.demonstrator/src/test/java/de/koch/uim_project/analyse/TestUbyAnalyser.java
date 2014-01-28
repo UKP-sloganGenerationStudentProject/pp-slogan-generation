306b371997df4479445585a2aed20f7c7a400105
Frerikko@gmail.com <Frerikko@gmail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-01-28 10:03:30 +0000

    ATTENTION: Not runable

--- trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/test/java/de/koch/uim_project/analyse/TestUbyAnalyser.java
+++ trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/test/java/de/koch/uim_project/analyse/TestUbyAnalyser.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import de.koch.uim_project.util.Config;
 import de.koch.uim_project.util.Constants;
 import de.koch.uim_project.util.DbConfig;
 
@@ -19,6 +21,10 @@ import de.tudarmstadt.ukp.lmf.model.enums.ERelTypeSemantics;
 
 public class TestUbyAnalyser {
 
+	
+	private DbConfig defaultConfig = new DbConfig(Constants.DATABASE.UBY.DEFAULT_UBY_URL, Constants.DATABASE.UBY.DEFAULT_UBY_LOGIN,
+			Constants.DATABASE.UBY.DEFAULT_UBY_PASS);
+	
 	@Test
 	public void testGetRelatedWo2rdsSense() throws DbException {
 		Word hot = new Word("hot", EPartOfSpeech.adjective);
@@ -31,8 +37,13 @@ public class TestUbyAnalyser {
 				hot,
 				relNames,
 				relTypes,
-				UbyConnect.getUbyInstance(new DbConfig(Constants.DATABASE.UBY.DEFAULT_UBY_URL, Constants.DATABASE.UBY.DEFAULT_UBY_LOGIN,
-						Constants.DATABASE.UBY.DEFAULT_UBY_PASS)));
+				UbyConnect.getUbyInstance(defaultConfig));
 		assertTrue(oxymerons.size() > 0);
+	}
+	
+	@Test
+	public void testGetPossibleMetaphers() throws DbException{
+		UbyAnalyser.getInstance().getPossibleMetaphors(Config.getDefaultConfig());
+		assert(true);
 	}
 }
