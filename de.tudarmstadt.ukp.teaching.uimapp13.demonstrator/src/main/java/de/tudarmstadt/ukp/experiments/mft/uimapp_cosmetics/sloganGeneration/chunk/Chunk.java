0b67a577f984f369db4fdd9af31ec367bd8f4585
roland.kluge.de@googlemail.com <roland.kluge.de@googlemail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-02-01 22:24:50 +0000

    Add Serializable interface

--- de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/experiments/mft/uimapp_cosmetics/sloganGeneration/chunk/Chunk.java
+++ de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/experiments/mft/uimapp_cosmetics/sloganGeneration/chunk/Chunk.java
@@ -1,5 +1,6 @@
 package de.tudarmstadt.ukp.experiments.mft.uimapp_cosmetics.sloganGeneration.chunk;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +10,10 @@ import de.tudarmstadt.ukp.experiments.mft.uimapp_cosmetics.sloganGeneration.chun
 import de.tudarmstadt.ukp.experiments.mft.uimapp_cosmetics.sloganGeneration.pattern.Pattern;
 import de.tudarmstadt.ukp.experiments.mft.uimapp_cosmetics.types.enumerations.ChunkType;
 
-
 public class Chunk
+    implements Serializable
 {
+    private static final long serialVersionUID = -6421940819649545578L;
     ChunkGeneric _genericForm;
     Pattern _containingPattern;
     ArrayList<ChunkPart> _chunkParts;
@@ -20,196 +22,183 @@ public class Chunk
     private boolean _hasConstraint;
     private boolean _haveConstraintsBeenChecked;
 
-
     public Chunk()
     {
-        _chunkParts = new ArrayList<ChunkPart>();
-        _mainChunkInd = -1;
-        _genericForm = new ChunkGeneric();
-        _generated = new ArrayList<>();
-        _containingPattern = null;
-        _hasConstraint = false;
-        _haveConstraintsBeenChecked = false;
+        this._chunkParts = new ArrayList<ChunkPart>();
+        this._mainChunkInd = -1;
+        this._genericForm = new ChunkGeneric();
+        this._generated = new ArrayList<>();
+        this._containingPattern = null;
+        this._hasConstraint = false;
+        this._haveConstraintsBeenChecked = false;
     }
 
-    public static Chunk createChunkOccurrence(ChunkType chunkType)
+    public static Chunk createChunkOccurrence(final ChunkType chunkType)
     {
         Chunk output = new Chunk();
 
-        if(chunkType.equals(ChunkType.NC))
-        {
+        if (chunkType.equals(ChunkType.NC)) {
             output = new NounChunk();
         }
 
         return output;
     }
 
-
-
-    public void addChunkPart(ChunkPart part)
+    public void addChunkPart(final ChunkPart part)
     {
-        _chunkParts.add(part);
+        this._chunkParts.add(part);
     }
 
-    public void setHeader(ChunkGeneric header)
+    public void setHeader(final ChunkGeneric header)
     {
-        _genericForm = header;
+        this._genericForm = header;
     }
 
     public ChunkGeneric getHeader()
     {
-        return _genericForm;
+        return this._genericForm;
     }
 
-    public void setContainingPattern(Pattern pattern)
+    public void setContainingPattern(final Pattern pattern)
     {
-        _containingPattern = pattern;
+        this._containingPattern = pattern;
     }
 
     public ChunkType getChunkType()
     {
-        return _genericForm.getChunkType();
+        return this._genericForm.getChunkType();
     }
 
     public String getSemantic()
     {
-        return _genericForm.getSemanticValue();
+        return this._genericForm.getSemanticValue();
     }
 
     public String getHeaderId()
     {
-        return _genericForm.getId();
+        return this._genericForm.getId();
     }
 
     public String getId()
     {
-        StringBuilder output = new StringBuilder();
+        final StringBuilder output = new StringBuilder();
 
-        for(ChunkPart occ : _chunkParts)
-        {
+        for (final ChunkPart occ : this._chunkParts) {
             output.append(occ.getTakenValue());
             output.append(" ");
         }
         return output.toString();
     }
 
-    public List<Chunk> getSimilarChunkOccurrences(Resources resources)
+    public List<Chunk> getSimilarChunkOccurrences(final Resources resources)
     {
         List<Chunk> output = new ArrayList<>();
-        if(_genericForm.getSemanticValue().equals("UNKNOWN"))
-        {
+        if (this._genericForm.getSemanticValue().equals("UNKNOWN")) {
             output.add(this);
         }
-        else
-        {
-            output = _genericForm.getOccurrences(resources);
+        else {
+            output = this._genericForm.getOccurrences(resources);
         }
         return output;
     }
 
-    public void generateInformation(Resources resources)
+    public void generateInformation(final Resources resources)
     {
     }
 
-
-    public List<ChunkSolution> generateChunkSolutions(Resources resources, Chunk originalChunk)
+    public List<ChunkSolution> generateChunkSolutions(final Resources resources,
+            final Chunk originalChunk)
     {
-        if(_generated.size()==0)
-        {
+        if (this._generated.size() == 0) {
 
             List<ChunkSolution> newChunkSolutions = new ArrayList<>();
-            ChunkSolution initialChunkSolution = new ChunkSolution(this);
+            final ChunkSolution initialChunkSolution = new ChunkSolution(this);
             newChunkSolutions.add(initialChunkSolution);
 
-            for(ChunkPart occ : _chunkParts)
-            {
-                List<ChunkSolution> newChunkSolutionsTemp = new ArrayList<>();
-                for(ChunkPart equiv : occ.getSimilarOccurrences(resources))
-                {
-                    List<ChunkPartSolution> generatedParts = equiv.generate(resources,occ);
-                    if(generatedParts.size()>0)
-                    {
-                        newChunkSolutionsTemp.addAll(ChunkSolution.concatenate(resources,newChunkSolutions,equiv.generate(resources,occ)));
+            for (final ChunkPart occ : this._chunkParts) {
+                final List<ChunkSolution> newChunkSolutionsTemp = new ArrayList<>();
+                for (final ChunkPart equiv : occ.getSimilarOccurrences(resources)) {
+                    final List<ChunkPartSolution> generatedParts = equiv.generate(resources, occ);
+                    if (generatedParts.size() > 0) {
+                        newChunkSolutionsTemp.addAll(ChunkSolution.concatenate(resources,
+                                newChunkSolutions, equiv.generate(resources, occ)));
                     }
                 }
                 newChunkSolutions = newChunkSolutionsTemp;
             }
 
-            _generated = newChunkSolutions;
+            this._generated = newChunkSolutions;
         }
 
-        return _generated;
+        return this._generated;
 
     }
 
     public int getPartsNbr()
     {
-        return _chunkParts.size();
+        return this._chunkParts.size();
     }
 
-    public ChunkPart getAt(int i)
+    public ChunkPart getAt(final int i)
     {
-        return _chunkParts.get(i);
+        return this._chunkParts.get(i);
     }
 
     public List<ChunkPart> getChunkParts()
     {
-        return _chunkParts;
+        return this._chunkParts;
     }
 
     public void resetCache()
     {
-        _generated = new ArrayList<>();
+        this._generated = new ArrayList<>();
     }
 
-    public void checkForGenericConstraints(Resources resources)
+    public void checkForGenericConstraints(final Resources resources)
     {
-        _genericForm.checkForConstraints(resources);
+        this._genericForm.checkForConstraints(resources);
     }
 
-    public void checkForConstraints(Resources resources)
+    public void checkForConstraints(final Resources resources)
     {
-        if(!_haveConstraintsBeenChecked)
-        {
-            for(ChunkPart part : _chunkParts)
-            {
+        if (!this._haveConstraintsBeenChecked) {
+            for (final ChunkPart part : this._chunkParts) {
                 part.checkForGenericConstraints(resources);
-                _hasConstraint = _hasConstraint || part.hasGenericConstraints();
+                this._hasConstraint = this._hasConstraint || part.hasGenericConstraints();
             }
-            _haveConstraintsBeenChecked = true;
+            this._haveConstraintsBeenChecked = true;
         }
 
     }
 
     public boolean hasConstraint()
     {
-        return _hasConstraint;
+        return this._hasConstraint;
     }
 
     public boolean hasGenericConstraint()
     {
-        return _genericForm.hasConstraint();
+        return this._genericForm.hasConstraint();
     }
 
     public void releaseConstraints()
     {
-        _hasConstraint = false;
-        _haveConstraintsBeenChecked = false;
+        this._hasConstraint = false;
+        this._haveConstraintsBeenChecked = false;
     }
 
     @Override
     public String toString()
     {
-        StringBuilder output = new StringBuilder();
+        final StringBuilder output = new StringBuilder();
         output.append(" [hasConstraint:");
-        output.append(_hasConstraint);
+        output.append(this._hasConstraint);
         output.append("]");
         output.append(" [haveConstraintsBeenChecked:");
-        output.append(_haveConstraintsBeenChecked);
+        output.append(this._haveConstraintsBeenChecked);
         output.append("]");
 
-        for(ChunkPart occ : _chunkParts)
-        {
+        for (final ChunkPart occ : this._chunkParts) {
             output.append(occ.getTakenValue());
             output.append(" ");
         }
@@ -217,20 +206,18 @@ public class Chunk
     }
 
     @Override
-    public boolean equals(Object obj)
+    public boolean equals(final Object obj)
     {
-        if(this == obj)
-        {
+        if (this == obj) {
             return true;
         }
-        if(!(obj instanceof Chunk))
-        {
+        if (!(obj instanceof Chunk)) {
             return false;
         }
 
-        Chunk occ = (Chunk) obj;
+        final Chunk occ = (Chunk) obj;
 
-        return toString().toLowerCase().equals(occ.toString().toLowerCase());
+        return this.toString().toLowerCase().equals(occ.toString().toLowerCase());
     }
 
 }
--- de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/experiments/mft/uimapp_cosmetics/sloganGeneration/chunk/NounChunk.java
+++ de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/experiments/mft/uimapp_cosmetics/sloganGeneration/chunk/NounChunk.java
@@ -8,51 +8,54 @@ import de.tudarmstadt.ukp.experiments.mft.uimapp_cosmetics.types.enumerations.Ch
 public class NounChunk
     extends Chunk
 {
+    private static final long serialVersionUID = 14702405733617685L;
     boolean _isBodyPart;
     String _bodyPartName;
 
     public NounChunk()
     {
         super();
-        _isBodyPart = false;
-        _bodyPartName = "UNKNOWN";
+        this._isBodyPart = false;
+        this._bodyPartName = "UNKNOWN";
     }
 
     @Override
-    public void generateInformation(Resources resources)
+    public void generateInformation(final Resources resources)
     {
 
         super.generateInformation(resources);
 
-        for(ChunkPart occPart : _chunkParts)
-        {
-            if(occPart.getChunkPartType().equals(ChunkPartType.N) && occPart.getSemanticValue().equals("body") && PatternGenerator.getSelectablePartsOfBody().contains(occPart.getLemma()) && PatternGenerator.getSelectablePartsOfBody().contains(occPart.getLemma()))
-            {
-                _isBodyPart = true;
-                _bodyPartName = occPart.getLemma();
+        for (final ChunkPart occPart : this._chunkParts) {
+            if (occPart.getChunkPartType().equals(ChunkPartType.N)
+                    && occPart.getSemanticValue().equals("body")
+                    && PatternGenerator.getSelectablePartsOfBody().contains(occPart.getLemma())
+                    && PatternGenerator.getSelectablePartsOfBody().contains(occPart.getLemma())) {
+                this._isBodyPart = true;
+                this._bodyPartName = occPart.getLemma();
             }
         }
     }
 
     public void setIsBodyPart()
     {
-        _isBodyPart = true;
+        this._isBodyPart = true;
     }
 
     public boolean isBodyPart()
     {
-        return _isBodyPart;
+        return this._isBodyPart;
     }
 
     public String getBodyPartName()
     {
-        return _bodyPartName;
+        return this._bodyPartName;
     }
 
     @Override
     public String toString()
     {
-        return super.toString()+" [ isBodyPart : "+_isBodyPart+"; bodyPartName : "+_bodyPartName+"]";
+        return super.toString() + " [ isBodyPart : " + this._isBodyPart + "; bodyPartName : "
+                + this._bodyPartName + "]";
     }
 
 }
