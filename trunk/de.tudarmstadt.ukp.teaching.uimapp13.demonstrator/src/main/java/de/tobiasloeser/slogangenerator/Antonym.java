2587ff75f717582a7c2625572bdaed7bab7b504d
roland.kluge.de@googlemail.com <roland.kluge.de@googlemail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-02-01 23:05:47 +0000

    Integrate Cars' SloganGenerator (in progress)

--- trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tobiasloeser/slogangenerator/Antonym.java
+++ trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tobiasloeser/slogangenerator/Antonym.java
@@ -1,5 +1,6 @@
 package de.tobiasloeser.slogangenerator;
 
+import java.io.Serializable;
 import java.util.List;
 
 import de.tudarmstadt.ukp.lmf.api.Uby;
@@ -10,53 +11,53 @@ import de.tudarmstadt.ukp.lmf.model.enums.ERelTypeSemantics;
 import de.tudarmstadt.ukp.lmf.model.semantics.Synset;
 import de.tudarmstadt.ukp.lmf.model.semantics.SynsetRelation;
 
-public class Antonym {
+public class Antonym
+    implements Serializable
+{
 
-	private Uby uby;
-	private Lexicon lex;
-	
-	public Antonym(Uby _uby) throws UbyInvalidArgumentException
-	{
-		uby = _uby;
-		lex = uby.getLexiconByName("OmegaWikieng");   
-	}
-	
-	public boolean HasAntonym(String word)
-	{
-		List<LexicalEntry> entries = uby.getLexicalEntries(word, lex);
-		for(LexicalEntry entry : entries)
-		{
-			List<Synset> synsets = entry.getSynsets();
-			for(Synset synset : synsets)
-			{
-				List<SynsetRelation> synsetRelations = synset.getSynsetRelations();
-				for(SynsetRelation synsetRelation : synsetRelations)
-				{
-					if(synsetRelation.getRelType().equals(ERelTypeSemantics.complementary))
-						return true;
-				}
-			}
-		} 
-		return false;
-	}
-	
-	public String GetAntonymAsSynset(String word)
-	{
-		List<LexicalEntry> entries = uby.getLexicalEntries(word, lex);
-		for(LexicalEntry entry : entries)
-		{
-			List<Synset> synsets = entry.getSynsets();
-			for(Synset synset : synsets)
-			{
-				List<SynsetRelation> synsetRelations = synset.getSynsetRelations();
-				for(SynsetRelation synsetRelation : synsetRelations)
-				{
-					if(synsetRelation.getRelType().equals(ERelTypeSemantics.complementary))
-						return synsetRelation.getTarget().getId();
-				}
-			}
-		} 
-		return null;
-	}
-	
+    private static final long serialVersionUID = 5241850566464965161L;
+    private Uby uby;
+    private Lexicon lex;
+
+    public Antonym(final Uby _uby)
+        throws UbyInvalidArgumentException
+    {
+        this.uby = _uby;
+        this.lex = this.uby.getLexiconByName("OmegaWikieng");
+    }
+
+    public boolean HasAntonym(final String word)
+    {
+        final List<LexicalEntry> entries = this.uby.getLexicalEntries(word, this.lex);
+        for (final LexicalEntry entry : entries) {
+            final List<Synset> synsets = entry.getSynsets();
+            for (final Synset synset : synsets) {
+                final List<SynsetRelation> synsetRelations = synset.getSynsetRelations();
+                for (final SynsetRelation synsetRelation : synsetRelations) {
+                    if (synsetRelation.getRelType().equals(ERelTypeSemantics.complementary)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public String GetAntonymAsSynset(final String word)
+    {
+        final List<LexicalEntry> entries = this.uby.getLexicalEntries(word, this.lex);
+        for (final LexicalEntry entry : entries) {
+            final List<Synset> synsets = entry.getSynsets();
+            for (final Synset synset : synsets) {
+                final List<SynsetRelation> synsetRelations = synset.getSynsetRelations();
+                for (final SynsetRelation synsetRelation : synsetRelations) {
+                    if (synsetRelation.getRelType().equals(ERelTypeSemantics.complementary)) {
+                        return synsetRelation.getTarget().getId();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
 }
