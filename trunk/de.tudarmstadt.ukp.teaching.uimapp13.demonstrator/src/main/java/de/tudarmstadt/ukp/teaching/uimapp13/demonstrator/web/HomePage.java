69d4db1fadcbef4dca02890e5955595cb799450c
roland.kluge.de@googlemail.com <roland.kluge.de@googlemail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-02-06 11:49:59 +0000

    Preparations to show previous generations

--- trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/teaching/uimapp13/demonstrator/web/HomePage.java
+++ trunk/de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/teaching/uimapp13/demonstrator/web/HomePage.java
@@ -3,6 +3,7 @@ package de.tudarmstadt.ukp.teaching.uimapp13.demonstrator.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -27,6 +28,8 @@ public class HomePage
 
     public HomePage(final List<Slogan> slogans, final String statusMessage)
     {
+        final ArrayList<List<Slogan>> previousSlogans = this.addToPreviousSlogans(slogans);
+
         this.add(new Label("message", statusMessage));
 
         this.add(new ProductDomainForm("productDomainForm", ProductDomain.BEAUTY));
@@ -35,7 +38,83 @@ public class HomePage
         this.add(new CarsForm("cars-form"));
         this.add(new GamesForm("games-form"));
 
-        this.add(new ListView<Slogan>("slogans", slogans)
+        final WebMarkupContainer resultsContainer = new WebMarkupContainer("results");
+        if (slogans.isEmpty()) {
+            resultsContainer.setVisible(false);
+        }
+        this.add(resultsContainer);
+
+        slogans.add(new Slogan("Life is Life, na na nanana."));
+        slogans.add(new Slogan("Creative automobile"));
+        slogans.add(new Slogan("Nothing is impossible"));
+
+        previousSlogans.add(slogans);
+
+        this.add(createSlogansList(slogans));
+
+        // <div id="previousResults">
+        // <h2>Previous Generations</h2>
+        // <ul wicket:id="previousResults">
+        // <li><h3>Results</h3>
+        // <ul class="slogans">
+        // <li>
+        // <div class="slogan">
+        // <span wicket:id="sloganText"></span>
+        // </div>
+        // </li>
+        // </ul></li>
+        // </ul>
+        // </div>
+        // this.add(new ListView<List<Slogan>>("previousResults", previousSlogans)
+        // {
+        // private static final long serialVersionUID = 1L;
+        //
+        // @Override
+        // protected void populateItem(final ListItem<List<Slogan>> item)
+        // {
+        // item.add(new ListView<Slogan>("slogans", item.getModel().getObject())
+        // {
+        //
+        // private static final long serialVersionUID = -8539671776074045601L;
+        //
+        // @Override
+        // protected void populateItem(final ListItem<Slogan> item)
+        // {
+        // final String text = item.getModel().getObject().getText();
+        // item.add(new Label("sloganText", String.format("#%d - %s", item.getIndex(),
+        // text)));
+        // }
+        // });
+        // }
+        // });
+
+    }
+
+    private ArrayList<List<Slogan>> addToPreviousSlogans(final List<Slogan> slogans)
+    {
+
+        @SuppressWarnings("unchecked")
+        ArrayList<List<Slogan>> allSlogans = (ArrayList<List<Slogan>>) this.getSession()
+                .getAttribute(SessionAttributes.PREVIOUS_SLOGANS);
+
+        if (allSlogans == null) {
+            allSlogans = new ArrayList<>();
+        }
+
+        final ArrayList<List<Slogan>> previousSlogans = allSlogans;
+
+        if (!slogans.isEmpty()) {
+            allSlogans.add(slogans);
+        }
+
+        this.getSession().setAttribute(SessionAttributes.PREVIOUS_SLOGANS, allSlogans);
+
+        return previousSlogans;
+    }
+
+    private static ListView<Slogan> createSlogansList(final List<Slogan> slogans)
+    {
+        return new ListView<Slogan>("slogans", slogans)
         {
 
             private static final long serialVersionUID = 1L;
@@ -43,9 +122,9 @@ public class HomePage
             @Override
             protected void populateItem(final ListItem<Slogan> item)
             {
-                item.add(new Label("sloganText", item.getModel().getObject().getText()));
+                final String text = item.getModel().getObject().getText();
+                item.add(new Label("sloganText", String.format("#%d - %s", item.getIndex(), text)));
             }
-        });
-
+        };
     }
 }
