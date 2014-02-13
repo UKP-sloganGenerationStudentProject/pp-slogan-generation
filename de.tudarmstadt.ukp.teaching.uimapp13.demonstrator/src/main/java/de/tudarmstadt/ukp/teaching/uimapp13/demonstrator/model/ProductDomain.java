98008fe9311284ab360b1cdd32b662a2f7d01fcd
roland.kluge.de@googlemail.com <roland.kluge.de@googlemail.com@d2fde074-4b45-a3a7-f639-fd1937fcae90>
2014-02-13 10:50:21 +0000

    Avoid configuration caching when the product domain of the cached configuration and the domain-specific form differ

--- de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/teaching/uimapp13/demonstrator/model/ProductDomain.java
+++ de.tudarmstadt.ukp.teaching.uimapp13.demonstrator/src/main/java/de/tudarmstadt/ukp/teaching/uimapp13/demonstrator/model/ProductDomain.java
@@ -38,4 +38,47 @@ public class ProductDomain
     {
         return ALL_DOMAINS;
     }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final ProductDomain other = (ProductDomain) obj;
+        if (this.id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        }
+        else if (!this.id.equals(other.id)) {
+            return false;
+        }
+        if (this.name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        }
+        else if (!this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
 }
